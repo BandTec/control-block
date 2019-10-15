f649bb91d408a0addb55622b8b96446947d01783
brunagd <bruna.garcia@bandtec.com.br>
2019-10-15 14:35:41 -0300

    validação do simulador

--- public/Novo site/Site Institucional/js/script.js
+++ public/Novo site/Site Institucional/js/script.js
@@ -1,11 +1,27 @@
 function calcular() {
+    
+    if(qtd_lojas.value == "" && qtd_caixas.value == "" && qtd_meses.value == ""){
+        Swal.fire({
+          type: 'error',
+          title: 'Tente novamente',
+          text: 'Preencha os campos corretamente!',
+        })
+    } else{
+
     let valor_arduino = 150 * Number(qtd_lojas.value);
     let valor_sensor = 30 * Number(qtd_caixas.value);
     let valor_nuvem = 450;
     let valor_manutencao = 150 * Number(qtd_meses.value);
     let valor_total = valor_arduino + valor_nuvem + valor_sensor;
-
-    total.innerHTML = valor_total;
-    manutencao.innerHTML = valor_manutencao;
-    orcamento.style.display="block"
-}
+    Swal.fire({
+        title: `O valor da compra de nossos serviços será de R$${valor_total}`,
+        text: `Você irá pagar uma mensalidade de R$ ${valor_manutencao}. por manutenção!
+         Contrantando o nosso serviço agora, você terá um desconto de 10% na contratação e a instalação será totalmente gratuita`,
+        imageUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/51/acceptation-1295324_960_720.png',
+        imageWidth: 120,
+        imageHeight: 120,
+        imageAlt: 'Custom image',
+        animation: false
+      })
+    }
+    }
