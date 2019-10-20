289e35351bdd7e2c01ba019f4922f016bd56429e
natanaelmelo <natanael.smelo@outlook.com>
2019-10-20 14:14:47 -0300

    site novo

--- public/Novo site/Site Institucional/js/script.js
+++ public/Novo site/Site Institucional/js/script.js
@@ -10,13 +10,13 @@ function calcular() {
 
     let valor_arduino = 150 * Number(qtd_lojas.value);
     let valor_sensor = 30 * Number(qtd_caixas.value);
-    let valor_nuvem = 450;
-    let valor_manutencao = 150 * Number(qtd_meses.value);
+    let valor_nuvem = 150;
+    let valor_manutencao = 150;
     let valor_total = valor_arduino + valor_nuvem + valor_sensor;
     Swal.fire({
         title: `O valor da compra de nossos serviços será de R$${valor_total}`,
-        text: `Você irá pagar uma mensalidade de R$ ${valor_manutencao}. por manutenção!
-         Contrantando o nosso serviço agora, você terá um desconto de 10% na contratação e a instalação será totalmente gratuita`,
+        text: `Você irá pagar uma mensalidade de R$ ${valor_manutencao} por manutenção!
+        Contrantando o nosso serviço agora, você terá um desconto de 10% na contratação e a instalação será totalmente gratuita`,
         imageUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/51/acceptation-1295324_960_720.png',
         imageWidth: 120,
         imageHeight: 120,
