d1b7fd80adf6ecf7a482eb68641c3d7b75db1a92
Ranyery <50641030+ranyery@users.noreply.github.com>
2019-11-25 13:14:07 -0300

    gráficos

--- projeto_node_local/insert-banco.js
+++ projeto_node_local/insert-banco.js
@@ -3,113 +3,115 @@
 // npm start
 
 // qual o nome da pasta onde está o node do site?
-var pasta_projeto_site = 'control-block';
+var pasta_projeto_site = "control-block";
 
 // leitura dos dados do Arduino
-var porta_serial = require('serialport');
+var porta_serial = require("serialport");
 var leitura_recebida = porta_serial.parsers.Readline;
 var banco = require(`../app-banco`);
 
 // prevenir problemas com muitos recebimentos de dados do Arduino
-require('events').EventEmitter.defaultMaxListeners = 15;
-
-
+require("events").EventEmitter.defaultMaxListeners = 15;
 
 function iniciar_escuta() {
+  porta_serial
+    .list()
+    .then(entradas_seriais => {
+      // este bloco trata a verificação de Arduino conectado (inicio)
 
-    porta_serial.list().then(entradas_seriais => {
+      var entradas_seriais_arduino = entradas_seriais.filter(entrada_serial => {
+        return (
+          entrada_serial.vendorId == 2341 && entrada_serial.productId == 43
+        );
+      });
 
-        // este bloco trata a verificação de Arduino conectado (inicio)
+      if (entradas_seriais_arduino.length != 1) {
+        throw new Error(
+          "Nenhum Arduino está conectado ou porta USB sem comunicação ou mais de um Arduino conectado"
+        );
+      }
 
-        var entradas_seriais_arduino = entradas_seriais.filter(entrada_serial => {
-            return entrada_serial.vendorId == 2341 && entrada_serial.productId == 43;
-        });
+      console.log(
+        "Arduino conectado na COM %s",
+        entradas_seriais_arduino[0].comName
+      );
 
-        if (entradas_seriais_arduino.length != 1) {
-            throw new Error("Nenhum Arduino está conectado ou porta USB sem comunicação ou mais de um Arduino conectado");
+      return entradas_seriais_arduino[0].comName;
+
+      // este bloco trata a verificação de Arduino conectado (fim)
+    })
+    .then(arduinoCom => {
+      // este bloco trata o recebimento dos dados do Arduino (inicio)
+
+      // o baudRate deve ser igual ao valor em
+      // Serial.begin(xxx) do Arduino (ex: 9600 ou 115200)
+      var arduino = new porta_serial(arduinoCom, {
+        baudRate: 9600
+      });
+
+      var parser = new leitura_recebida();
+      arduino.pipe(parser);
+
+      console.error("Iniciando escuta do Arduino");
+
+      // Tudo dentro desse parser.on(...
+      // é invocado toda vez que chegarem dados novos do Arduino
+      parser.on("data", dados => {
+        console.error(`Recebeu novos dados do Arduino: ${dados}`);
+        try {
+          var leitura = dados.split(",");
+          registrar_leitura(Number(leitura[0]), Number(leitura[1]));
+        } catch (e) {
+          throw new Error(`Erro ao tratar os dados recebidos do Arduino: ${e}`);
         }
 
-        console.log("Arduino conectado na COM %s", entradas_seriais_arduino[0].comName);
-
-        return entradas_seriais_arduino[0].comName;
-
-
-        // este bloco trata a verificação de Arduino conectado (fim)
-
-    }).then(arduinoCom => {
-
-        // este bloco trata o recebimento dos dados do Arduino (inicio)
-
-        // o baudRate deve ser igual ao valor em
-        // Serial.begin(xxx) do Arduino (ex: 9600 ou 115200)
-        var arduino = new porta_serial(arduinoCom, {
-            baudRate: 9600
-        });
-
-        var parser = new leitura_recebida();
-        arduino.pipe(parser);
-
-        console.error('Iniciando escuta do Arduino');
-
-        // Tudo dentro desse parser.on(...
-        // é invocado toda vez que chegarem dados novos do Arduino
-        parser.on('data', (dados) => {
-            console.error(`Recebeu novos dados do Arduino: ${dados}`);
-            try {
-                var leitura = dados.split(',');
-                registrar_leitura(Number(leitura[0]), Number(leitura[1]));
-            } catch (e) {
-                throw new Error(`Erro ao tratar os dados recebidos do Arduino: ${e}`);
-            }
-
-            // este bloco trata o recebimento dos dados do Arduino (fim)
-        });
-
-    }).catch(error => console.error(`Erro ao receber dados do Arduino ${error}`));
+        // este bloco trata o recebimento dos dados do Arduino (fim)
+      });
+    })
+    .catch(error => console.error(`Erro ao receber dados do Arduino ${error}`));
 }
 
 // função que recebe valores de Entrada e Saída
 // e faz um insert no banco de dados
 function registrar_leitura(Entrada) {
+  if (efetuando_insert) {
+    console.log("Execução em curso. Aguardando 10s...");
+    setTimeout(() => {
+      registrar_leitura(Entrada);
+    }, 2000);
 
-    if (efetuando_insert) {
-        console.log('Execução em curso. Aguardando 10s...');
-        setTimeout(() => {
-            registrar_leitura(Entrada);
-        }, 2000);
+    return;
+  }
 
-        return;
-    }
+  efetuando_insert = true;
 
-        efetuando_insert = true;
+  console.log(`tipo_sensor_entrada: '${Entrada}'`);
 
-        console.log(`tipo_sensor_entrada: '${Entrada}'`);
-
-        banco.conectar().then(pool => {
-
-            return pool.request().query(`insert into eventos (tipo_sensor_entrada) values ('${Entrada}');
-             select idevento as id from eventos where idevento = fksensores;`);
-
-        }).catch(err => {
-
-            var erro = `Erro ao tentar registrar aquisição na base: ${err}`;
-            console.error(erro);
-
-        }).finally(() => {
-            banco.sql.close();
-            efetuando_insert = false;
-        });
-
-    }
+  banco
+    .conectar()
+    .then(pool => {
+      return pool.request()
+        .query(`insert into eventos (evento) values ('${Entrada}');
+             select evento from eventos;`);
+    })
+    .catch(err => {
+      var erro = `Erro ao tentar registrar aquisição na base: ${err}`;
+      console.error(erro);
+    })
+    .finally(() => {
+      banco.sql.close();
+      efetuando_insert = false;
+    });
+}
 
 var efetuando_insert = false;
 
 // iniciando a "escuta" de dispositivos Arduino
 iniciar_escuta();
 
-// por enquanto está comentado mas está função gera números aleatórios 
+// por enquanto está comentado mas está função gera números aleatórios
 
 //setInterval(function() {
-  //      registrar_leitura(Math.floor((Math.random() * ( 1+ 1)) + 17), 
-    //                 Math.floor((Math.random() * (1+ 1)) + 10))
- //}, 10000);
+//      registrar_leitura(Math.floor((Math.random() * ( 1+ 1)) + 17),
+//                 Math.floor((Math.random() * (1+ 1)) + 10))
+//}, 10000);
--- public/routes/leituras.js
+++ public/routes/leituras.js
@@ -1,67 +1,75 @@
 // não mexa nestas 3 linhas!
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var banco = require('../../app-banco');
+var banco = require("../../app-banco");
 // não mexa nessas 3 linhas!
-// aqui acontece os registros das últimas leiturass 
-router.get('/ultimas', function(req, res, next) {
-    console.log(banco.conexao);
-    banco.conectar().then(pool => {
-        var limite_linhas = 1;
-        return pool.request().query(`select top ${limite_linhas} 
-                            tipo_sensor_entrada,
-                            tipo_sensor_saida,
-                            FORMAT(tempo,'HH:mm:ss') as eventos
-                            from eventos order by idEventos desc`);
-    }).then(consulta => {
-        // o formato da hora é HH: Hora com 2 dígitos  MM: Minutos com 2 dígitos  SS: Segundos com 2 dígitos, selecionar os dados de forma descrecente
-        // é considerado o melhor jeito porque pega os últimos registros considerando que a intenção é pegar os últimos registros 
-        console.log(`Resultado da consulta: ${JSON.stringify(consulta.recordset)}`);
-        res.send(consulta.recordset);
-
-    }).catch(err => {
-
-        var erro = `Erro na leitura dos últimos registros: ${err}`;
-        console.error(erro);
-        res.status(500).send(erro);
-
-    }).finally(() => {
-        banco.sql.close();
+// aqui acontece os registros das últimas leiturass
+router.get("/ultimas", function(req, res, next) {
+  console.log(banco.conexao);
+  banco
+    .conectar()
+    .then(pool => {
+      var limite_linhas = 1;
+      return pool.request().query(`SELECT TOP ${limite_linhas} 
+                            tipo_sensor,
+                            hora_evento,
+                            evento,
+                            data_evento
+                            from EVENTOS order by idEventos DESC`);
+    })
+    .then(consulta => {
+      // o formato da hora é HH: Hora com 2 dígitos  MM: Minutos com 2 dígitos  SS: Segundos com 2 dígitos, selecionar os dados de forma descrecente
+      // é considerado o melhor jeito porque pega os últimos registros considerando que a intenção é pegar os últimos registros
+      console.log(
+        `Resultado da consulta: ${JSON.stringify(consulta.recordset)}`
+      );
+      res.send(consulta.recordset);
+    })
+    .catch(err => {
+      var erro = `Erro na leitura dos últimos registros: ${err}`;
+      console.error(erro);
+      res.status(500).send(erro);
+    })
+    .finally(() => {
+      banco.sql.close();
     });
-
 });
 
-
 // dados em tempo real
-router.get('/tempo-real', function(req, res, next) {
-    console.log(banco.conexao);
+router.get("/tempo-real", function(req, res, next) {
+  console.log(banco.conexao);
 
-    var estatisticas = {
-        Entrada: 0,
-        Saida: 0
-    };
+  var estatisticas = {
+    Entrada: 0,
+    Saida: 0
+  };
 
-    banco.conectar().then(() => {
-        return banco.sql.query(`
-        select top 1 tipo_sensor_entrada, tipo_sensor_saida from eventos order by id desc
-        `);
-    }).then(consulta => {
-        // "select top" limita as linhas retornadas em um conjunto de resultados que começão em 1, em ordem descrecente dos dados das estastiticas
+  banco
+    .conectar()
+    .then(() => {
+      return banco.sql.query(`SELECT TOP ${limite_linhas} 
+      tipo_sensor,
+      hora_evento,
+      evento,
+      data_evento
+      from EVENTOS order by idEventos DESC`);
+    })
+    .then(consulta => {
+      // "select top" limita as linhas retornadas em um conjunto de resultados que começão em 1, em ordem descrecente dos dados das estastiticas
 
-        estatisticas.Entrada = consulta.recordset[0].Entrada;
-        estatisticas.saida = consulta.recordset[0].saida;
-        console.log(`Tempo real: ${JSON.stringify(estatisticas)}`);
+      estatisticas.Entrada = consulta.recordset[0].Entrada;
+      estatisticas.saida = consulta.recordset[0].saida;
+      console.log(`Tempo real: ${JSON.stringify(estatisticas)}`);
 
-        res.send(estatisticas);
-
-    }).catch(err => {
-
-        var erro = `Erro na leitura dos registros de tempo real: ${err}`;
-        console.error(erro);
-        res.status(500).send(erro);
-
-    }).finally(() => {
-        banco.sql.close();
+      res.send(estatisticas);
+    })
+    .catch(err => {
+      var erro = `Erro na leitura dos registros de tempo real: ${err}`;
+      console.error(erro);
+      res.status(500).send(erro);
+    })
+    .finally(() => {
+      banco.sql.close();
     });
 });
 
